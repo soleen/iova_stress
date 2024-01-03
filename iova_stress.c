259a25d44eb5fa26538aa41daf4cc70f51a09f0a
Pasha Tatashin <pasha.tatashin@soleen.com>
2024-01-03 21:15:13 +0000

    find iommu group automatically, and use err instead perror

--- iova_stress.c
+++ iova_stress.c
@@ -6,14 +6,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 #include <linux/vfio.h>
 #include <sys/sysinfo.h>
 #include <sys/param.h>
 #include <time.h>
+#include <err.h>
 
-#define IOMMU_GROUP_DEFAULT		83
 #define IOVA_SPACE_SIZE_DEFAULT		45
 #define DMA_SIZE			sysconf(_SC_PAGE_SIZE)
 #define NSEC				1000000000ul
@@ -55,8 +56,8 @@ static void dma_map_unmap(int container, unsigned long iova_space,
 					MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
 
 	if ((void *)dma_map.vaddr == MAP_FAILED) {
-		perror("mmap");
-		exit(1);
+		err(EXIT_FAILURE, "mmap(0, dma_size=%ld, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, ...)",
+		    dma_size);
 	}
 
 	dma_unmap.size = dma_map.size = dma_size;
@@ -77,19 +78,16 @@ static void dma_map_unmap(int container, unsigned long iova_space,
 			/* might be pre-existing entries, ignore */
 			continue;
 		}
-		if (ioctl(container, VFIO_IOMMU_UNMAP_DMA, &dma_unmap)) {
-			fprintf(stderr, "VFIO_IOMMU_UNMAP_DMA failed at 0x%lx\n", dma_unmap.iova);
-			exit(1);
-		}
+		if (ioctl(container, VFIO_IOMMU_UNMAP_DMA, &dma_unmap))
+			err(EXIT_FAILURE, "VFIO_IOMMU_UNMAP_DMA failed at 0x%lx", dma_unmap.iova);
 	}
 
 	munmap((void *)dma_map.vaddr, dma_size);
 }
 
-int
-main (int argc, char *argv[]) {
+int main (int argc, char *argv[]) {
 	int container, group, opt, verbose = 0;
-	int iommu_group = IOMMU_GROUP_DEFAULT;
+	int iommu_group = -1;
 	unsigned long iova_space = IOVA_SPACE_SIZE_DEFAULT;
 	struct vfio_group_status group_status = { .argsz = sizeof(group_status) };
 	long freeram_before, freeram_after, iommu;
@@ -118,38 +116,40 @@ main (int argc, char *argv[]) {
 		case 'h':
 			rv = EXIT_SUCCESS;
 		default: /* '?' */
-			fprintf(stderr, HELP_STR, argv[0],
-				IOVA_SPACE_SIZE_DEFAULT);
-			exit(rv);
+			errx(rv, HELP_STR, argv[0], IOVA_SPACE_SIZE_DEFAULT);
 		}
 	}
 
 	/* Create a new container */
 	container = open("/dev/vfio/vfio", O_RDWR);
-	if (container < 0) {
-		perror("open(\"/dev/vfio/vfio\"");
-		exit(rv);
-	}
+	if (container < 0)
+		err(rv, "open(%s)", "/dev/vfio/vfio");
 
 	/* Open the group */
-	sprintf(group_path, "/dev/vfio/%d", iommu_group);
-	group = open(group_path, O_RDWR);
-	if (group < 0) {
-		perror("open(group_path)");
-		exit(rv);
+	if (iommu_group < 0) {
+		for (iommu_group = 0; iommu_group < 512; iommu_group++) {
+			sprintf(group_path, "/dev/vfio/%d", iommu_group);
+			group = open(group_path, O_RDWR);
+			if (group >= 0)
+				break;
+		}
+		if (iommu_group == 512)
+			errx(rv, "Can't find an group under /dev/vfio/[0-511]");
+	} else {
+		sprintf(group_path, "/dev/vfio/%d", iommu_group);
+		group = open(group_path, O_RDWR);
+		if (group < 0)
+			err(rv, "open(%s, O_RDWR)", group_path);
 	}
+	printf("Using %s\n", group_path);
 
 	/* Add the group to the container */
-	if (ioctl(group, VFIO_GROUP_SET_CONTAINER, &container)) {
-		perror("ioctl(group, VFIO_GROUP_SET_CONTAINER, &container)");
-		exit(rv);
-	}
+	if (ioctl(group, VFIO_GROUP_SET_CONTAINER, &container))
+		err(rv, "ioctl(group=%d, VFIO_GROUP_SET_CONTAINER, ...)", group);
 
 	/* Enable the IOMMU model we want */
-	if (ioctl(container, VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU)) {
-		perror("ioctl(container, VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU)");
-		exit(rv);
-	}
+	if (ioctl(container, VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU))
+		err(rv, "ioctl(container=%d, VFIO_SET_IOMMU, VFIO_TYPE1_IOMMU)", container);
 
 	dma_size = first_dma_size;;
 	sysinfo(&info);
